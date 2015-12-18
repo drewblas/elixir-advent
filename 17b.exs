c633791008a006b489e628dc35b6cee9254ca7a3
Drew Blas <drew.blas@gmail.com>
2015-12-18 10:33:43 -0600

    Day 17 complete

---
+++ 17b.exs
@@ -0,0 +1,32 @@
+defmodule Seventeen do
+  @volume 150
+  def go do
+    IO.stream(:stdio, :line)
+    |> Stream.map(&String.strip/1)
+    |> Enum.map(&process/1)
+    |> solve
+  end
+
+  def process(line) do
+    line |> String.to_integer
+  end
+
+  def solve(containers), do: solve(1, containers)
+
+  def solve(n, containers) do
+    cnt = combination(n, containers) |> Enum.count(fn(c) -> Enum.sum(c) == @volume end)
+
+    case cnt do
+      0 -> solve(n+1, containers)
+      _ -> {n, cnt}
+    end
+  end
+
+  def combination(0, _), do: [[]]
+  def combination(_, []), do: []
+  def combination(n, [h|t]) do
+    (for x <- combination(n-1, t), do: [h|x]) ++ combination(n, t)
+  end
+end
+
+IO.puts inspect Seventeen.go
